bcf47f49b29aaf47878fd4aaa991d30a866c240f
Christian Sheen <christiansheen@MacBook-Pro.local>
2020-08-31 22:08:42 -0400

    In index.js add "callbackURL" key + second argument to GoogleStrategy, which is a fucntion

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys");
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ hi: "Buddy" });
-});
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken) => {
+      console.log(accessToken);
+    },
+  ),
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
