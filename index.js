c5bfa627d10a01bdf8d0b1323d98e4ee771396c3
Christian Sheen <christiansheen@MacBook-Pro.local>
2020-08-31 22:34:37 -0400

    create express second routeHandler which is responsible for handling the "auth/google/callback" get route, while using google strategy to parse for url for return code to continue oath flow

--- index.js
+++ index.js
@@ -18,5 +18,16 @@ passport.use(
   ),
 );
 
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  }),
+);
+
+app.get("/auth/google/callback", passport.authenticate("google"));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+
+//https://accounts.google.com/o/oauth2/v2/auth/oauthchooseaccount?response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Fauth%2Fgoogle%2Fcallback&scope=profile%20email&client_id=135996444907-sdl68qan93pnmivls1m109peh5fmnslf.apps.googleusercontent.com&flowName=GeneralOAuthFlow
