4b4817495ad0681065a6c258b2d6cdf184ca0628
Christian Sheen <christiansheen@MacBook-Pro.local>
2020-09-02 11:06:04 -0400

    learn how to spell PRCOESSgaa

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,8 @@
 // figure out what set of credentials to return
-if (proccess.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production") {
   // we are in production - return production keys
   module.exports = require("./prod");
 } else {
   // we are in development - return development keys
-  module.exports = require("./dev");
+  module.exports = require("/dev");
 }
--- config/prod.js
+++ config/prod.js
@@ -1,8 +1,8 @@
 module.exports = {
-  googleClientID: proccess.env.GOOGLE_CLIENT_ID,
-  googleClientSecret: proccess.env.GOOGLE_CLIENT_SECRET,
-  mongoURI: proccess.env.MONGO_URI,
-  cookieKey: proccess.env.COOKIE_KEY,
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY,
 };
 
 // clientID: "685896259857 - veahra58th6s5icm7hundanfgu5rumu7.apps.googleusercontent.com;"
