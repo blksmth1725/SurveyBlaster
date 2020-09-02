c0d6afbfff2ee045a885642655e0fbe5b421818d
Christian Sheen <christiansheen@MacBook-Pro.local>
2020-09-02 09:14:23 -0400

    add two new config files. refactor api/database keys so that there are two enviornment key sets. production & development. .gitignore dev.js to maintain privacy. assign prod keys to proccess.env to determine which enviornment the keys belong to

---
+++ config/prod.js
@@ -0,0 +1,9 @@
+module.exports = {
+  googleClientID: proccess.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: proccess.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: proccess.env.MONGO_URI,
+  cookieKey: proccess.env.COOKIE_KEY,
+};
+
+// clientID: "685896259857 - veahra58th6s5icm7hundanfgu5rumu7.apps.googleusercontent.com;"
+// clientSecret: "wIpB2pvd2u6JGorfAAYqi1Pw"
