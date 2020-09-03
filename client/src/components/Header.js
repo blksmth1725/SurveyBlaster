9fd44d7e85073ddbf136a9ba90e80afcf625a228
Christian Sheen <christiansheen@MacBook-Pro.local>
2020-09-03 10:00:07 -0400

    create store with redux, applyMiddleware (redux-thunk) to handle side effects (http requests). make sure action creator handles api request via axios, dispatch obj of type FETCH_USER response = DATA. Wire up app component to connect and recieve all redux store state as props for use

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,26 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class Header extends Component {
   render() {
-    return <div>Header</div>;
+    console.log(this.props);
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <a className="left brand-logo">Emaily</a>
+          <ul className="right">
+            <li>
+              <a>Login with Google</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
   }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
