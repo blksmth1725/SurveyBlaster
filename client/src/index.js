4c41b30deb64fdf7a38cff2870ca4a35a22858d3
Christian Sheen <christiansheen@MacBook-Pro.local>
2020-09-04 09:54:49 -0400

    post rebase

--- client/src/index.js
+++ client/src/index.js
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
-=======
 import "materialize-css/dist/css/materialize.min.css";
 import React from "react";
 import ReactDOM from "react-dom";
@@ -35,4 +16,3 @@ ReactDOM.render(
   </Provider>,
   document.querySelector("#root"),
 );
->>>>>>> 5e972e90a7167d57dd909b07c09eb62d3641e2c9
