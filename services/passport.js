cafdf06ec4af34b6c061c2c644138cd2c2925e95
Christian Sheen <christiansheen@MacBook-Pro.local>
2020-09-04 17:07:36 -0400

    testing server

--- services/passport.js
+++ services/passport.js
@@ -24,10 +24,10 @@ passport.use(
       proxy: true,
     },
     async (accessToken, refreshToken, profile, done) => {
-      const existingUser = await User.findOne({ googleId: profile.id });
-      if (existingUser) {
-        return done(null, existingUser);
-      }
+      // const existingUser = await User.findOne({ googleId: profile.id });
+      // if (existingUser) {
+      //   return done(null, existingUser);
+      // }
 
       const user = await new User({
         googleId: profile.id,
