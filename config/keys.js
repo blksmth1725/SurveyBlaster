4682b110dca3c7d4d878faef64b5337094189b9f
Christian Sheen <christiansheen@MacBook-Pro.local>
2020-09-03 22:00:35 -0400

    add client side file structure

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 // figure out what set of credentials to return
-if (proccess.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production") {
   // we are in production - return production keys
   module.exports = require("./prod");
 } else {
