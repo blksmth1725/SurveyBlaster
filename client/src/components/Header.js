1405e65cfb6f47ccb5cf7eaf49e42fa9ac7ce5eb
Christian Sheen <christiansheen@MacBook-Pro.local>
2020-09-05 20:20:04 -0400

    moving forward, working on making left header component clickable to redirect to root route and modify landing component

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -15,7 +15,7 @@ class Header extends Component {
       default:
         return (
           <li>
-            <a>Logout</a>
+            <a href="/api/logout">Logout</a>
           </li>
         );
     }
