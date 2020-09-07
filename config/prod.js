cbd8520e0a772c7ce76260764004496008d5bfea
Christian Sheen <christiansheen@MacBook-Pro.local>
2020-09-06 23:25:45 -0400

    update npm engine version

--- config/prod.js
+++ config/prod.js
@@ -3,6 +3,8 @@ module.exports = {
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
+  stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+  stripeSecretKey: process.env.STRIPE_SECRET_KEY,
 };
 
 // clientID: "685896259857 - veahra58th6s5icm7hundanfgu5rumu7.apps.googleusercontent.com;"
