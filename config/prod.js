05070be38f310e7302dd44a01df6d123c396cd5b
Christian Sheen <christiansheen@MacBook-Pro.local>
2020-09-19 13:40:22 -0400

    integrate all of sendgrid as to allow users to send recipients surveys in the form of email templates. WARNING: went too long without a commit

--- config/prod.js
+++ config/prod.js
@@ -5,6 +5,7 @@ module.exports = {
   cookieKey: process.env.COOKIE_KEY,
   stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
   stripeSecretKey: process.env.STRIPE_SECRET_KEY,
+  sendGridKey: process.env.SEND_GRID_KEY,
 };
 
 // clientID: "685896259857 - veahra58th6s5icm7hundanfgu5rumu7.apps.googleusercontent.com;"
--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const cookieSession = require("cookie-session");
 const passport = require("passport");
 const bodyParser = require("body-parser");
 const keys = require("./config/keys");
-require("./models/user");
+require("./models/User");
+require("./models/Survey");
 require("./services/passport");
 
 mongoose.connect(keys.mongoURI, {
@@ -27,6 +28,7 @@ app.use(passport.session());
 
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
+require("./routes/surveyRoutes")(app);
 
 if (process.env.NODE_ENV === "production") {
   // making sure that express serves up production assets
