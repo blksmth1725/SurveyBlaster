e65943a473a6fce9c02eff5b896f7485eeacc253
Christian Sheen <christiansheen@MacBook-Pro.local>
2020-10-09 14:37:00 -0400

    Casing

--- client/src/components/surveys/surveyForm.js
+++ client/src/components/surveys/surveyForm.js
@@ -3,7 +3,7 @@ import _ from "lodash";
 import React, { Component } from "react";
 import { reduxForm, Field } from "redux-form";
 import { Link } from "react-router-dom";
-import SurveyField from "./surveyField";
+import SurveyField from "./SurveyField";
 import validateEmails from "../../utils/validateEmails";
 
 const FIELDS = [
