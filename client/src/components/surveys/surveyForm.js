635e18fe5f120e958daeb1db7646235c48be0743
Christian Sheen <christiansheen@MacBook-Pro.local>
2020-10-11 17:51:27 -0400

    create formFields.js and extract FIELDS form SurveyForm.js replace all intances of FIELDS with formFields... then in SurveyFormReview add reviewFields constant which you map over to recieve current value inpusts from SurveyForm

--- client/src/components/surveys/surveyForm.js
+++ client/src/components/surveys/surveyForm.js
@@ -5,17 +5,11 @@ import { reduxForm, Field } from "redux-form";
 import { Link } from "react-router-dom";
 import SurveyField from "./SurveyField";
 import validateEmails from "../../utils/validateEmails";
-
-const FIELDS = [
-  { label: "Survey title", name: "title" },
-  { label: "Subject line", name: "subject" },
-  { label: "Email body", name: "body" },
-  { label: "Recipients list", name: "emails" },
-];
+import formFields from "./formFields";
 
 class SurveyForm extends Component {
   renderFields() {
-    return _.map(FIELDS, ({ label, name }) => {
+    return _.map(formFields, ({ label, name }) => {
       return (
         <Field
           key={name}
@@ -50,7 +44,7 @@ function validate(values) {
 
   errors.emails = validateEmails(values.emails || "");
 
-  _.each(FIELDS, ({ name }) => {
+  _.each(formFields, ({ name }) => {
     if (!values[name]) {
       errors[name] = "You must provide adequete input";
     }
