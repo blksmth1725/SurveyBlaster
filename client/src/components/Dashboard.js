42841e759a933a27cf46b1a34a0104d8cbf076ae
Christian Sheen <christiansheen@MacBook-Pro.local>
2020-10-24 15:22:26 -0400

    completed Survey Blaster, ready to deploy.... stripe connectivity issues due to lack of live stripe API key

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,15 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import SurveyList from "./surveys/SurveyList";
 
 const Dashboard = () => {
   return (
     <div>
-      Dashboard
+      <SurveyList />
       <div className="fixed-action-button" style={{ marginTop: "30px" }}>
-        <Link
-          to="/surveys/new"
-          className="btn-floating btn-large waves-effect waves-light #26a69a"
-        >
+        <Link to="/surveys/new" className="btn-floating btn-large #26a69a">
           <i className="material-icons">add</i>
         </Link>
       </div>
--- models/Survey.js
+++ models/Survey.js
@@ -10,7 +10,7 @@ const surveySchema = new Schema({
   yes: { type: Number, default: 0 },
   no: { type: Number, default: 0 },
   _user: { type: Schema.Types.ObjectId, ref: "User" },
-  date: Date,
+  dateSent: Date,
   lastResponded: Date,
 });
 
